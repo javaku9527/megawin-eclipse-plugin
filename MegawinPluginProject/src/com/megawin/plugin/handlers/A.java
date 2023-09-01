e489c174cbd3cd4312978904bcef35f513c85742
eric_ku <eric_ku@gss.com.tw>
2023-09-01 17:24:16 +0800

    123

---
+++ MegawinPluginProject/src/com/megawin/plugin/handlers/A.java
@@ -0,0 +1,8 @@
+package com.megawin.plugin.handlers;
+
+import org.eclipse.debug.core.IDebugEventSetListener;
+import org.eclipse.debug.core.IMemoryBlockListener;
+
+public class A extends VariablesView implements IMemoryBlockListener, IDebugEventSetListener {
+
+}
